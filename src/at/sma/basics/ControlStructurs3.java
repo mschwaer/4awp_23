793a30e0b9e3ee7e6de86ffa0d2d4ad7b0f4363f
Markus Schwaerzler <markus.schwaerzler@htldornbirn.at>
2023-10-18 10:47:59 +0200

    ControlStructurs4 erstellt

--- src/at/sma/basics/ControlStructurs3.java
+++ src/at/sma/basics/ControlStructurs3.java
@@ -20,8 +20,16 @@ public class ControlStructurs3 {
         int randomNumber = random.nextInt(100);
 
         // Gib die Zufallszahl aus
-        System.out.println(randomNumber);
+
         // Wenn die Zahl kleiner ist als 20  gib aus "Mini"
+        if (randomNumber < 20){
+            System.out.println("Mini");
+        } else if (randomNumber > 20 && randomNumber < 50) {
+            System.out.println("Medium");
+        } else if (randomNumber > 50 ){
+            System.out.println("Large");
+        }
+        System.out.println(randomNumber);
         // Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
         // Wenn die Zahl größer als 50 ist gib aus "Large"
 
---
+++ src/at/sma/basics/XXXX.java
@@ -0,0 +1,13 @@
+package at.sma.basics;
+
+public class XXXX {
+    public static void main(String[] args) {
+        // Erstelle eine Zufallszahl zwischen 5 und 10
+
+        // Wenn der Wert 10 ist gib aus Ten
+        // Wenn der Wert 9 ist gib aus Nine
+        // Wenn der Wert 8 ist gib aus Eight
+        // etc.
+
+    }
+}
