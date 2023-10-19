669f8bb2cff83e52d02498495e35d7fbf5d3dbff
Markus Schwaerzler <markus.schwaerzler@htldornbirn.at>
2023-10-19 16:22:10 +0200

    ControlStructurs4 erstellt

--- src/TestIT.java
+++ src/TestIT.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class TestIT {
     public static void main(String[] args) {
-        System.out.println(Main.add(2,3));
+
+
+        Scanner scanner = new Scanner(System.in);
+        int auswahl =0;
+
+        do{
+            System.out.print("Bitte den Wert eingeben(mit 4 ist schluss):");
+            auswahl = scanner.nextInt();
+            System.out.println("Hier kommt der Wert:"+ auswahl);
+
+        } while (auswahl != 4);
+
+
+
     }
 }
